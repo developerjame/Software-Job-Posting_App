b9fd138302894157bfa9c1d5d07dc3cc13473529
developerjame <119099522+developerjame@users.noreply.github.com>
2024-01-07 17:28:41 +0300

    commit

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -83,7 +83,7 @@
            {{$slot}}
         </main>
         <footer
-        class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-primary text-black h-24 mt-24 opacity-90 md:justify-center"
+        class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-primary text-white h-24 mt-24 opacity-90 md:justify-center"
     >
         <p class="ml-2">Copyright &copy; 2023, All Rights reserved</p>
 
